ea611d3f009add3f9fc37c5d20e799d4bd5afdd4
Sara Negoceki <sarapnegoceki@MacBook-Air-de-Sara.local>
2025-10-14 18:07:37 -0300

    game completed

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <script setup>
+  import { ref } from 'vue';
   import MemoryGame from './components/MemoryGame.vue';
   import PlayerName from './components/PlayerName.vue';
+  import FinishGame from './components/FinishGame.vue';
 
-  // const startGame = localStorage.getItem('player') ? true : false;
+  const startGame = ref(false);
+  const finishGame = ref(false);
+
+  function updatePlayerName() {
+    startGame.value = localStorage.getItem('player') ? true : false;
+  }
+
+  function restartGame() {
+    startGame.value = false;
+    finishGame.value = true;
+  }
+
+  function startNewGame() {
+    finishGame.value = false;
+    startGame.value = true;
+  }
 </script>
 
 <template>
   <section class="container">
     <h1>Memory game</h1>
-    <PlayerName></PlayerName>
-    <MemoryGame></MemoryGame>
+    <PlayerName @update:setPlayerName="updatePlayerName()"></PlayerName>
+    <MemoryGame v-show="startGame" @update:restartGame="restartGame()"></MemoryGame>
+    <FinishGame v-show="finishGame" @update:startNewGame="startNewGame()"></FinishGame>
   </section>
 </template>
 
--- src/components/MemoryCard.vue
+++ src/components/MemoryCard.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
 const props = defineProps({
     idCard: String
 });
+const emit = defineEmits(['update:score']);
 
 function showCard(event) {
     const card = event.target.closest('.card');
@@ -9,22 +13,23 @@ function showCard(event) {
     const front = card.querySelector('.card-front');
     front.classList.toggle('show');
 
-    compareCards(card.id);
+    compareCards({id: card.id, content: front.textContent.trim()});
 }
 
 function compareCards(card){
-    let lastCard = localStorage.getItem('cardInPlay');
+    const lastCard = localStorage.getItem('lastCard') ? JSON.parse(localStorage.getItem('lastCard')) : {id: null, content: null};
 
-    if(!lastCard) {
-        localStorage.setItem('cardInPlay', card);
+    if(lastCard.id === null && lastCard.content === null) {
+        lastCard.id = card.id;
+        lastCard.content = card.content;
+        localStorage.setItem('lastCard', JSON.stringify(lastCard));
     } else {
-        if(lastCard === card){
-            console.log('Match!');
+        if(lastCard.content === card.content && lastCard.id !== card.id){
+            emit('update:score');
         } else {
-            console.log('No match!');
             setTimeout(() => {
-                const lastCardElement = document.getElementById(lastCard);
-                const currentCardElement = document.getElementById(card);
+                const lastCardElement = document.getElementById(lastCard.id);
+                const currentCardElement = document.getElementById(card.id);
 
                 if(lastCardElement && currentCardElement){
                     lastCardElement.querySelector('.card-front').classList.remove('show');
@@ -33,7 +38,7 @@ function compareCards(card){
             }, 1000);
         }
 
-        localStorage.removeItem('cardInPlay');
+        localStorage.removeItem('lastCard');
     }
 }
 </script>
@@ -48,7 +53,7 @@ function compareCards(card){
     </div>
 </template>
 
-<style>
+<style scoped>
     .card{
         width: 100px;
         height: 100px;
